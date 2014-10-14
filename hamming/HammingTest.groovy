142a277715a649d62947a9f051b94532c92f7f6d
Rick Briganti <jeviolle@newaliases.org>
2014-10-13 22:22:57 -0400

    Removed groovy extends

--- hamming/HammingTest.groovy
+++ hamming/HammingTest.groovy
@@ -1,7 +1,7 @@
 import org.junit.Test
 import static org.junit.Assert.assertEquals
 
-class HammingTest extends GroovyTestCase {
+class HammingTest {
   @Test
   void testIdenticalStrands() {
     assertEquals 0, new Hamming().compute('A','A')
