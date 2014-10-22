6398dba60a5b351475e58e1531491863b9a5faf2
Rick Briganti <jeviolle@newaliases.org>
2014-10-21 23:22:47 -0400

    added nth-prime

---
+++ nth-prime/Example.groovy
@@ -0,0 +1,35 @@
+import groovy.transform.CompileStatic
+
+@CompileStatic
+class Prime {
+
+  private def isPrime(int n) {
+    def sqrt = Math.ceil(Math.sqrt(n))
+    for (int i = 3; i <= sqrt; i += 1 ) {
+      if ( n % i == 0 ) {
+        return false
+      }  
+    }
+    return true
+  }
+
+  def nth(int n) {
+    // handle stuff that doesn't need
+    // additional calculations
+    if ( n < 1 ) throw new ArithmeticException()
+    if ( n == 1 ) return 2
+
+    def lastValue = 0
+    def count = 1
+    def i = 3
+
+    while ( count < n ) {
+      if ( isPrime(i) ) {
+        lastValue = i
+        count += 1
+      }
+      i += 2
+    }
+    return lastValue
+  }
+}
