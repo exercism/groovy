a16740102970dce28c86efcbd97386c5f7d0b0b2
Rick Briganti <jeviolle@newaliases.org>
2014-10-25 16:07:54 -0400

    Added roman-numerals

---
+++ roman-numerals/Example.groovy
@@ -0,0 +1,33 @@
+class Roman {
+  def Roman() {
+    Integer.metaClass.toRoman = {->
+      def romanMappings = [
+        1000:"M", 
+        900:"CM", 
+        500:"D",
+        400:"CD", 
+        100:"C", 
+        90:"XC",
+        50:"L", 
+        40:"XL", 
+        10:"X",
+        9:"IX", 
+        5:"V", 
+        4:"IV", 
+        1:"I"
+      ]
+
+      def roman = new String()
+      def num = delegate
+
+      romanMappings.each() { k, v ->
+        while (num >= k) {
+          roman += v
+          num -= k
+        }
+      }
+
+      roman
+    }
+  }
+}
