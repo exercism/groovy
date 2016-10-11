4bb382bc5ea09be569a544c4c5250bee879b643f
Jake Gage <jake.gage@gmail.com>
2016-10-11 16:22:59 +0200

    Implement RNA Transcription test as Spock specification
    - Fix indentation
    - Make test cases match canonical
    - Implement IllegalArgumentException, per canonical data

--- exercises/rna-transcription/Example.groovy
+++ exercises/rna-transcription/Example.groovy
@@ -1,20 +1,25 @@
 class Complement {
-  private def dnaMap = ["G":"C","C":"G","T":"A","A":"U"]
-  private def rnaMap = ["C":"G","G":"C","A":"T","U":"A"]
 
-  private def transcribe(seq, seqMap) {
-    def result = new String()
-    seq.split('').each() {
-      result += seqMap[it] 
+    private def dnaMap = ["G":"C","C":"G","T":"A","A":"U"]
+    private def rnaMap = ["C":"G","G":"C","A":"T","U":"A"]
+
+    private def transcribe(seq, seqMap) {
+        def result = new String()
+        seq.split('').each() {
+            if ( !seqMap[it] ) {
+                throw new IllegalArgumentException()
+            }
+            result += seqMap[it]
+        }
+        return result
     }
-    return result
-  }
 
-  def ofDNA(strand) {
-    transcribe(strand, dnaMap)   
-  }
+    def ofDNA(strand) {
+        transcribe(strand, dnaMap)
+    }
 
-  def ofRNA(strand) {
-    transcribe(strand, rnaMap) 
-  }
+    def ofRNA(strand) {
+        transcribe(strand, rnaMap)
+    }
+
 }
