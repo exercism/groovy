2219e5fed43b0a89faee82a5bb7df36e68893f78
Matthew Moss <mattmoss@splatbang.com>
2017-10-09 16:45:12 -0500

    binary-search: add comment to exercise file on assumption that can be made

--- exercises/binary-search/BinarySearch.groovy
+++ exercises/binary-search/BinarySearch.groovy
@@ -2,6 +2,7 @@ class BinarySearch {
 
     List data
 
+    // You may assume for this exercise that the input data is sorted in ascending order.
     BinarySearch(List data) {
         this.data = data
     }
