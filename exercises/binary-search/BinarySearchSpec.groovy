e4aaf7f7cab5e7b86bcca72883b056060ec04b7c
Matthew Moss <mattmoss@splatbang.com>
2017-10-09 16:59:37 -0500

    binary-search: review spec, fix bugs

--- exercises/binary-search/BinarySearchSpec.groovy
+++ exercises/binary-search/BinarySearchSpec.groovy
@@ -5,52 +5,52 @@ class BinarySearchSpec extends Specification {
 
     def "finds a value in an array with one element"() {
         expect:
-        BinarySearch(6).indexOf(6) == 0
+        new BinarySearch([6]).indexOf(6) == 0
     }
 
     def "finds a value in the middle of an array"() {
         expect:
-        BinarySearch(1, 3, 4, 6, 8, 9, 11).indexOf(6) == 3
+        new BinarySearch([1, 3, 4, 6, 8, 9, 11]).indexOf(6) == 3
     }
 
     def "finds a value at the beginning of an array"() {
         expect:
-        BinarySearch(1, 3, 4, 6, 8, 9, 11).indexOf(1) == 0
+        new BinarySearch([1, 3, 4, 6, 8, 9, 11]).indexOf(1) == 0
     }
 
     def "finds a value at the end of an array"() {
         expect:
-        BinarySearch(1, 3, 4, 6, 8, 9, 11).indexOf(11) == 6
+        new BinarySearch([1, 3, 4, 6, 8, 9, 11]).indexOf(11) == 6
     }
 
     def "finds a value in an array of odd length"() {
         expect:
-        BinarySearch(1, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 634).indexOf(144) == 9
+        new BinarySearch([1, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 634]).indexOf(144) == 9
     }
 
     def "finds a value in an array of even length"() {
         expect:
-        BinarySearch(1, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377).indexOf(21) == 5
+        new BinarySearch([1, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377]).indexOf(21) == 5
     }
 
     def "identifies that a value is not included in the array"() {
         expect:
-        BinarySearch(1, 3, 4, 6, 8, 9, 11).indexOf(7) == 0
+        new BinarySearch([1, 3, 4, 6, 8, 9, 11]).indexOf(7) == -1
     }
 
-    def "a value smaller than the array's smallest value is not included'() {
+    def "a value smaller than the array's smallest value is not included"() {
         expect:
-        BinarySearch(1, 3, 4, 6, 8, 9, 11).indexOf(0) == -1
+        new BinarySearch([1, 3, 4, 6, 8, 9, 11]).indexOf(0) == -1
     }
 
     def "a value larger than the array's largest value is not included"() {
         expect:
-        BinarySearch(1, 3, 4, 6, 8, 9, 11).indexOf(13) == -1
+        new BinarySearch([1, 3, 4, 6, 8, 9, 11]).indexOf(13) == -1
     }
 
     def "nothing is included in an empty array"() {
         expect:
-        BinarySearch().indexOf(1) == -1
+        new BinarySearch([]).indexOf(1) == -1
     }
 
 }
