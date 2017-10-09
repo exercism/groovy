31154ab88c53e3abdeea493289dcf4f426bdf171
Matthew Moss <mattmoss@splatbang.com>
2017-10-09 16:41:40 -0500

    binary-search: add exercise file

---
+++ exercises/binary-search/BinarySearch.groovy
@@ -0,0 +1,13 @@
+class BinarySearch {
+
+    List data
+
+    BinarySearch(List data) {
+        this.data = data
+    }
+
+    int indexOf(item) {
+        throw new UnsupportedOperationException('indexOf method not implemented')
+    }
+
+}
