6a40d5ad3b80726b46828d06f8a42ce0059c22a3
Ilya Khadykin <mageofit@gmail.com>
2017-10-11 23:25:09 +0300

    Create skeleton for the exercise secret-handshake

---
+++ exercises/secret-handshake/Example.groovy
@@ -0,0 +1,6 @@
+class SecretHandshake {
+
+    static List<String> decodeBinaryNumber(int number) {
+        throw new UnsupportedOperationException('method not implemented.')
+    }
+}
---
+++ exercises/secret-handshake/SecretHandshake.groovy
@@ -0,0 +1,9 @@
+class SecretHandshake {
+
+    static List<String> decodeBinaryNumber(Integer number) {
+        // TODO: Pass all test cases
+        if ( number == 1) {
+            return Collections.singletonList("wink")
+        }
+    }
+}
