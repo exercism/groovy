adab769166f064278b65dbf78a5ea05be3283d6e
Mattia Tommasone <mtommasone@ebay.com>
2017-11-27 15:28:25 +0100

    Ported the linked-list exercise from java

---
+++ exercises/linked-list/DoubleLinkedList.groovy
@@ -0,0 +1,19 @@
+class DoubleLinkedList<T> {
+
+    void push(T value) {
+        throw new UnsupportedOperationException('Method implementation is missing')
+    }
+
+    T pop() {
+        throw new UnsupportedOperationException('Method implementation is missing')
+    }
+
+    T shift() {
+        throw new UnsupportedOperationException('Method implementation is missing')
+    }
+
+    void unshift(T value) {
+        throw new UnsupportedOperationException('Method implementation is missing')
+    }
+
+}
