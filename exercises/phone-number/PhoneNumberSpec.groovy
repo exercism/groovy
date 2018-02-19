d7743d973f19e6b0396e6666666a88aaa4919c79
Chuck Daniels <cjdaniels4@gmail.com>
2018-02-18 23:12:56 -0500

    phone-number: Update tests to match problem specs
    Add tests to disallow 0 and 1 as first digit of area code or exchange
    and update example to pass new tests. Also, fix typo in README.md.
    
    Resolves #110

--- exercises/phone-number/PhoneNumberSpec.groovy
+++ exercises/phone-number/PhoneNumberSpec.groovy
@@ -2,74 +2,87 @@
 import spock.lang.*
 
 class PhoneNumberSpec extends Specification {
-
-    def 'a phone number is valid'() {
-        expect: new PhoneNumber('(123) 456-7890').number == '1234567890'
+    def 'cleans the number'() {
+        expect: new PhoneNumber('(223) 456-7890').number == '2234567890'
     }
 
     @Ignore
-    def 'a different phone number is valid'() {
-        expect: new PhoneNumber('(987) 654-3210').number == '9876543210'
+    def 'cleans numbers with dots'() {
+        expect: new PhoneNumber('223.456.7890').number == '2234567890'
     }
 
     @Ignore
-    def 'a phone nubmer separated by dots is valid'() {
-        expect: new PhoneNumber('456.123.7890').number == '4561237890'
+    def 'cleans numbers with multiple spaces'() {
+        expect: new PhoneNumber('223 456   7890   ').number == '2234567890'
     }
 
     @Ignore
-    def 'a letter-containing phone number is invalid'() {
-        expect: new PhoneNumber('123-abc-1234').number == '0000000000'
-    }
-
-    @Ignore
-    def 'a 9-digit number is invalid'() {
+    def 'invalid when 9 digits'() {
         expect: new PhoneNumber('123456789').number == '0000000000'
     }
 
     @Ignore
-    def 'an 11-digit number starting with a 1 is valid'() {
-        expect: new PhoneNumber('19876543210').number == '9876543210'
+    def 'invalid when 11 digits does not start with a 1'() {
+        expect: new PhoneNumber('22234567890').number == '0000000000'
     }
 
     @Ignore
-    def 'a 10-digit number, with an area code starting with 1, is valid'() {
-        expect: new PhoneNumber('1234567890').number == '1234567890'
+    def 'valid when 11 digits and starting with 1'() {
+        expect: new PhoneNumber('12234567890').number == '2234567890'
     }
 
     @Ignore
-    def 'an 11-digit number, not beginning with a 1, is invalid'(){
-        expect: new PhoneNumber('21234567890').number == '0000000000'
+    def 'valid when 11 digits and starting with 1 even with punctuation'() {
+        expect: new PhoneNumber('+1 (223) 456-7890').number == '2234567890'
     }
 
     @Ignore
-    def 'a 12-digit number, even one beginning with a 1, is invalid'(){
-        expect: new PhoneNumber('112345678901').number == '0000000000'
+    def 'invalid when more than 11 digits'() {
+        expect: new PhoneNumber('321234567890').number == '0000000000'
     }
 
     @Ignore
-    def 'a 10-digit number containing letters is inavlid'() {
-        expect: new PhoneNumber('1a2a3a4a5a6a7a8a9a0a').number == '0000000000'
+    def 'invalid with letters'() {
+        expect: new PhoneNumber('123-abc-7890').number == '0000000000'
     }
 
     @Ignore
-    def 'can extract area code from a phone number'() {
-        expect: new PhoneNumber('1234567890').areaCode == '123'
+    def 'invalid with punctuations'() {
+        expect: new PhoneNumber('123-@:!-7890').number == '0000000000'
     }
 
     @Ignore
-    def 'can extract an area code from a different phone number'() {
+    def 'invalid if area code starts with 0'() {
+        expect: new PhoneNumber('(023) 456-7890').number == '0000000000'
+    }
+
+    @Ignore
+    def 'invalid if area code starts with 1'() {
+        expect: new PhoneNumber('(123) 456-7890').number == '0000000000'
+    }
+
+    @Ignore
+    def 'invalid if exchange code starts with 0'() {
+        expect: new PhoneNumber('(223) 056-7890').number == '0000000000'
+    }
+
+    @Ignore
+    def 'invalid if exchange code starts with 1'() {
+        expect: new PhoneNumber('(223) 156-7890').number == '0000000000'
+    }
+
+    @Ignore
+    def 'can extract an area code from a phone number'() {
         expect: new PhoneNumber('9876543210').areaCode == '987'
     }
 
     @Ignore
     def 'can pretty print a phone number'() {
-        expect: new PhoneNumber('5551234567').toString() == '(555) 123-4567'
+        expect: new PhoneNumber('5552234567').toString() == '(555) 223-4567'
     }
 
     @Ignore
     def 'can pretty print a full U.S. phone number'() {
-        expect: new PhoneNumber('11234567890').toString() == '(123) 456-7890'
+        expect: new PhoneNumber('12234567890').toString() == '(223) 456-7890'
     }
-
 }
