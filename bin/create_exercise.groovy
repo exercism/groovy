283ee70c1f5a704e94a28099ba5f953264f74289
Anthony Scotti <anthony.m.scotti@gmail.com>
2019-09-02 15:45:54 -0400

    Upgrades Gradle to 5.6.1 and Groovy to 2.5.8

--- bin/create_exercise.groovy
+++ bin/create_exercise.groovy
@@ -19,7 +19,7 @@ repositories {
 
 dependencies {
     testCompile "org.spockframework:spock-core:1.3-groovy-2.5"
-    compile "org.codehaus.groovy:groovy-all:2.5.7"
+    compile "org.codehaus.groovy:groovy-all:2.5.8"
 }
 
 test {
