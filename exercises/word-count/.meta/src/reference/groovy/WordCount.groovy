13920dfdb4311ee023400e1858747899ce9f0658
Anthony Scotti <anthony.m.scotti@gmail.com>
2019-10-27 18:24:34 -0400

    Add missing version files

--- exercises/word-count/.meta/src/reference/groovy/WordCount.groovy
+++ exercises/word-count/.meta/src/reference/groovy/WordCount.groovy
@@ -1,13 +1,12 @@
 class WordCount {
-    private static final REPLACE_PATTERN = ~/[^\w']|'\B|\B'/
-
-    String sentence
+    private final REPLACE_PATTERN = ~/[^\w']|'\B|\B'/
+    private final String sentence
 
     WordCount(String sentence) {
         this.sentence = sentence
     }
 
-    def wordCount() {
+    def countWords() {
         sentence.toLowerCase()
                 .replaceAll(REPLACE_PATTERN, ' ')
                 .tokenize()
