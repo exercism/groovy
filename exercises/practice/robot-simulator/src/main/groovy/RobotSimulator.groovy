942902ab5535212de80a23b45ded8cceb8502ddf
András B Nagy <20251272+BNAndras@users.noreply.github.com>
2024-01-30 23:53:39 -0800

    Add robot-simulator exercise (#382)
    * Add robot-simulator exercise
    
    * Increase robot-simulator difficulty
    
    * Update test suite

---
+++ exercises/practice/robot-simulator/src/main/groovy/RobotSimulator.groovy
@@ -0,0 +1,9 @@
+class RobotSimulator {
+    RobotSimulator(int pos_x, int pos_y, String direction) {
+        throw new UnsupportedOperationException('Constructor implementation is missing')
+    }
+
+    def move(String commands) {
+        throw new UnsupportedOperationException('Move implementation is missing')
+    }
+}
