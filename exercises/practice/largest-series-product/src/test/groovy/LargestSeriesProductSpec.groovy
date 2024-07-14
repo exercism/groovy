9c0889c515a813203e27e5f7e4dd0e1f282dcd45
Максим Толстиков <tolsms@gmail.com>
2024-07-14 14:18:34 +0300

    Unified the "where:" blocks

--- exercises/practice/largest-series-product/src/test/groovy/LargestSeriesProductSpec.groovy
+++ exercises/practice/largest-series-product/src/test/groovy/LargestSeriesProductSpec.groovy
@@ -3,90 +3,90 @@ import spock.lang.*
 class LargestSeriesProductSpec extends Specification {
     def "finds the largest product if span equals length"() {
         expect:
-        LargestSeriesProduct.largestProduct(digits, span) == expected
+        LargestSeriesProduct.largestProduct(digits, span) == expectedResult
 
         where:
-        digits | span || expected
+        digits | span || expectedResult
         "29"   | 2    || 18
     }
 
     @Ignore
     def "can find the largest product of 2 with numbers in order"() {
         expect:
-        LargestSeriesProduct.largestProduct(digits, span) == expected
+        LargestSeriesProduct.largestProduct(digits, span) == expectedResult
 
         where:
-        digits       | span || expected
+        digits       | span || expectedResult
         "0123456789" | 2    || 72
     }
 
     @Ignore
     def "can find the largest product of 2"() {
         expect:
-        LargestSeriesProduct.largestProduct(digits, span) == expected
+        LargestSeriesProduct.largestProduct(digits, span) == expectedResult
 
         where:
-        digits      | span || expected
+        digits      | span || expectedResult
         "576802143" | 2    || 48
     }
 
     @Ignore
     def "can find the largest product of 3 with numbers in order"() {
         expect:
-        LargestSeriesProduct.largestProduct(digits, span) == expected
+        LargestSeriesProduct.largestProduct(digits, span) == expectedResult
 
         where:
-        digits       | span || expected
+        digits       | span || expectedResult
         "0123456789" | 3    || 504
     }
 
     @Ignore
     def "can find the largest product of 3"() {
         expect:
-        LargestSeriesProduct.largestProduct(digits, span) == expected
+        LargestSeriesProduct.largestProduct(digits, span) == expectedResult
 
         where:
-        digits       | span || expected
+        digits       | span || expectedResult
         "1027839564" | 3    || 270
     }
 
     @Ignore
     def "can find the largest product of 5 with numbers in order"() {
         expect:
-        LargestSeriesProduct.largestProduct(digits, span) == expected
+        LargestSeriesProduct.largestProduct(digits, span) == expectedResult
 
         where:
-        digits       | span || expected
+        digits       | span || expectedResult
         "0123456789" | 5    || 15120
     }
 
     @Ignore
     def "can get the largest product of a big number"() {
         expect:
-        LargestSeriesProduct.largestProduct(digits, span) == expected
+        LargestSeriesProduct.largestProduct(digits, span) == expectedResult
 
         where:
-        digits                                               | span || expected
+        digits                                               | span || expectedResult
         "73167176531330624919225119674426574742355349194934" | 6    || 23520
     }
 
     @Ignore
     def "reports zero if the only digits are zero"() {
         expect:
-        LargestSeriesProduct.largestProduct(digits, span) == expected
+        LargestSeriesProduct.largestProduct(digits, span) == expectedResult
 
         where:
-        digits | span || expected
+        digits | span || expectedResult
         "0000" | 2    || 0
     }
 
     @Ignore
     def "reports zero if all spans include zero"() {
         expect:
-        LargestSeriesProduct.largestProduct(digits, span) == expected
+        LargestSeriesProduct.largestProduct(digits, span) == expectedResult
 
         where:
-        digits  | span || expected
+        digits  | span || expectedResult
         "99099" | 3    || 0
     }
 
@@ -97,30 +97,30 @@ class LargestSeriesProductSpec extends Specification {
 
         then:
         IllegalArgumentException exceptionThrown = thrown(IllegalArgumentException)
-        exceptionThrown.message == "span must be smaller than string length"
+        exceptionThrown.message == expectedErrorMessage
 
         where:
-        digits | span
-        "123"  | 4
+        digits | span || expectedErrorMessage
+        "123"  | 4    || "span must be smaller than string length"
     }
 
     @Ignore
     def "reports 1 for empty string and empty product (0 span)"() {
         expect:
-        LargestSeriesProduct.largestProduct(digits, span) == expected
+        LargestSeriesProduct.largestProduct(digits, span) == expectedResult
 
         where:
-        digits | span || expected
+        digits | span || expectedResult
         ""     | 0    || 1
     }
 
     @Ignore
     def "reports 1 for nonempty string and empty product (0 span)"() {
         expect:
-        LargestSeriesProduct.largestProduct(digits, span) == expected
+        LargestSeriesProduct.largestProduct(digits, span) == expectedResult
 
         where:
-        digits | span || expected
+        digits | span || expectedResult
         "123"  | 0    || 1
     }
 
@@ -131,11 +131,11 @@ class LargestSeriesProductSpec extends Specification {
 
         then:
         IllegalArgumentException exceptionThrown = thrown(IllegalArgumentException)
-        exceptionThrown.message == "span must be smaller than string length"
+        exceptionThrown.message == expectedErrorMessage
 
         where:
-        digits | span
-        ""     | 1
+        digits | span || expectedErrorMessage
+        ""     | 1    || "span must be smaller than string length"
     }
 
     @Ignore
@@ -145,11 +145,11 @@ class LargestSeriesProductSpec extends Specification {
 
         then:
         IllegalArgumentException exceptionThrown = thrown(IllegalArgumentException)
-        exceptionThrown.message == "digits input must only contain digits"
+        exceptionThrown.message == expectedErrorMessage
 
         where:
-        digits   | span
-        "1234a5" | 2
+        digits   | span || expectedErrorMessage
+        "1234a5" | 2    || "digits input must only contain digits"
     }
 
     @Ignore
@@ -159,10 +159,10 @@ class LargestSeriesProductSpec extends Specification {
 
         then:
         IllegalArgumentException exceptionThrown = thrown(IllegalArgumentException)
-        exceptionThrown.message == "span must not be negative"
+        exceptionThrown.message == expectedErrorMessage
 
         where:
-        digits  | span
-        "12345" | -1
+        digits  | span || expectedErrorMessage
+        "12345" | -1   || "span must not be negative"
     }
 }
