2a68825ff8c8905b778006866c2c44e273d59887
Glenn Jackman <glenn.jackman@gmail.com>
2025-10-24 18:07:35 -0400

    sync largest-series-product tests

--- exercises/practice/largest-series-product/.meta/src/reference/groovy/LargestSeriesProduct.groovy
+++ exercises/practice/largest-series-product/.meta/src/reference/groovy/LargestSeriesProduct.groovy
@@ -1,7 +1,7 @@
 class LargestSeriesProduct {
     static int largestProduct(String digits, final int span) {
         if (span < 0) throw new IllegalArgumentException("span must not be negative")
-        if (span > digits.length()) throw new IllegalArgumentException("span must be smaller than string length")
+        if (span > digits.length()) throw new IllegalArgumentException("span must not exceed string length")
         if (!digits.chars().allMatch(Character::isDigit)) throw new IllegalArgumentException("digits input must only contain digits")
         int maximum = 0
         for (i in 0..digits.length() - span) {
